7c2c8a746320a83bfcb797bfb891f6c15adc9480
inspireena <saini.reena962@gmail.com>
2023-03-20 17:08:54 +0530

    fixed phaser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Testing from "./components/Testing.jsx";
 
 function App() {
     return (
-        <div >
-            Hello World ####
-        </div>
+        <>
+            <div >
+                Hello World ####
+            </div>
+            <Testing />
+        </>
     );
 }
 export default App;
---
+++ src/components/Testing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+function Testing() {
+    return (
+        <div>
+            <Helmet>
+                {/* <script src="/public/phaser.min.js"></script> */}
+                <script src="/public/plugins.js"></script>
+            </Helmet>
+        </div>
+    );
+}
+export default Testing;
